494ce9c8e6dad768cd3e88589528b5111833306c
Cooper b. Anderson <andersc7@rose-hulman.edu>
2023-02-14 11:45:44 -0500

    Write script to store 100 runs

---
+++ experiments/store.py
@@ -0,0 +1,15 @@
+import os
+from tqdm import tqdm
+
+exp = (False, False)
+mapping = "cyclical"
+
+def growing(value: bool) -> str:
+    return "growing" if value else "adult"
+
+for i in tqdm(range(100)):
+    cmd = "cargo run --release -- --nogui > "
+    cmd += "brain-" + growing(exp[0])
+    cmd += "_body-" + growing(exp[1])
+    cmd += "/data/" + mapping + str(i)
+    os.system(cmd)
